2eafe1c509856e8bfe8726de0b3a5cb22f1d4c8a
gracezhang01 <gulianxiao@berkeley.edu>
2025-05-09 00:54:09 -0400

    let it work

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,14 @@ export default defineConfig({
 	plugins: [react(), reactComponentTagger()],
 	build: {
 		chunkSizeWarningLimit: 10240,
+		rollupOptions: {
+			output: {
+				manualChunks: undefined,
+				assetFileNames: 'assets/[name].[hash][extname]',
+				chunkFileNames: 'assets/[name].[hash].js',
+				entryFileNames: 'assets/[name].[hash].js',
+			},
+		},
 	},
 	server: {
 		proxy: {
